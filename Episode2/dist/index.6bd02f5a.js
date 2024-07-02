547248046f468697be5257fcea04be6183c9c456
Sameer <sameerkpathanofficial@gmail.com>
2024-07-02 10:06:18 +0530

    Episode-02 | Igniting Our App

--- Episode2/dist/index.6bd02f5a.js
+++
@@ -1,5 +0,0 @@
-const heading = React.createElement("h2", {}, "Welcome");
-const root = ReactDOM.createRoot(document.getElementById("main"));
-root.render(heading);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
