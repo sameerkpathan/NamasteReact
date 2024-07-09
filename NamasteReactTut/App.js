1c723d7c47da655d0a9dd7e2dd6d60f8d3f6fbbc
Sameer <sameerkpathanofficial@gmail.com>
2024-07-09 16:09:15 +0530

    Episode 3 | Laying The Foundation

--- NamasteReactTut/App.js
+++ NamasteReactTut/App.js
@@ -14,18 +14,54 @@ const heading = React.createElement(
 );
 
 //Due to this Code complexity devloper in facebook created JSX(Javascript xml)
-//so JSX is not a html inside js 
-//JSX is HTML like syntax or xml Like syntax 
-
+//so JSX is not a html inside js
+//JSX is HTML like syntax or xml Like syntax
 
 // JSX ==>babel transpile in to ==> React.createElement() ==> ReactElement - JS Object ==> HTMLElementRender
 
 const jsxheading = <h1>Welcome in JSX world</h1>;
 
 // console.log(heading);
-console.log(jsxheading)
+console.log(jsxheading);
+
+//React Functional Component
+//A function which return a react element is called function component
+
+const FunctionComp = function () {
+  return (
+    <div>
+      <h1>Using Normal Function </h1>
+    </div>
+  );
+};
+
+const HeadingFun = () => {
+  return (
+    <div id="mainheading">
+      <h1>Namaste React Function</h1>
+    </div>
+  );
+};
+
+const elm = <span>Element</span>;
+
+const mainelm = <h1>react element and sub {elm}</h1>;
+
+const number = 1000;
+
+const Title = () => {
+  return (
+    <div>
+      <FunctionComp />
+      {number} and {mainelm}
+      <h1>Title </h1>
+      <HeadingFun></HeadingFun>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("main"));
 
 //so below render function can be replaced whatever you written in main div file and render react Element
-root.render(jsxheading);
+// root.render(jsxheading);
+root.render(<Title />);
