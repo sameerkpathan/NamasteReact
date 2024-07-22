e34a5b595b6da2e0030e8fffcab169eaacbdb507
Sameer <sameerkpathanofficial@gmail.com>
2024-07-23 00:34:04 +0530

    Episode6 | Exploring the world

--- Episode6/src/Component/Body.js
+++ Episode6/src/Component/Body.js
@@ -1,6 +1,7 @@
 import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import ShimmerUi from "./ShimmerUi";
+import { Swiggy_URL } from "../Utils/constant";
 
 const Body = () => {
   const [RestolistItem, setRestolistItem] = useState([]);
@@ -8,30 +9,35 @@ const Body = () => {
   const [filteredRestro, setFilteredRestro] = useState([]);
   const [searchRestro, setSearchRestro] = useState("");
 
+  const [RestaurantName, setRestaurantName] = useState("");
+
   //whenever state variable update react trigger the reconcilation process(it will re-render component)
 
   console.log("Body Re-render");
 
-//it gives a cors error when you are starting this app and try to fetch API so that time you can install an cors extension in chrome and turn on that extension so it can bypass that headers and will run in local app.
+  //it gives a cors error when you are starting this app and try to fetch API so that time you can install an cors extension in chrome and turn on that extension so it can bypass that headers and will run in local app.
+
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-    const json = await data.json();
-    // console.log(json);
+    try {
+      
+      const data = await fetch(Swiggy_URL);
+      const json = await data.json();
 
-    //optional chaining
-
-    setRestolistItem(
-      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-    setFilteredRestro(
-      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
+      setRestolistItem(
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      setFilteredRestro(
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+    } catch (error) {
+      console.error("Error fetching Data :", error);
+    }
   };
 
   //conditional rendering
@@ -39,12 +45,36 @@ const Body = () => {
   //   return <ShimmerUi/>;
   // }
 
+  const handleSearch = () => {
+    //Filter the Restorent here and update the UI
+    //search
+
+    const Searchrestrobyinput = RestolistItem.filter((RestoItem) =>
+      RestoItem.info.name.toLowerCase().includes(searchRestro.toLowerCase())
+    );
+    setFilteredRestro(Searchrestrobyinput);
+    setSearchRestro(""); //to clear  the search input
+    setRestaurantName(searchRestro);
+  };
+
+  const handleTopRestro = () => {
+    const FilterRestorent = filteredRestro.filter((Items) => {
+      return Items.info.avgRating >= 4.5;
+    });
+    setFilteredRestro(FilterRestorent);
+  };
+
+  const handleAllList = () => {
+    setFilteredRestro(RestolistItem);
+  };
+
   return RestolistItem.length === 0 ? (
     <ShimmerUi />
   ) : (
     <div className="body">
       <div className="filter">
         <div className="search">
+
           <input
             type="text"
             className="search-box"
@@ -54,49 +84,37 @@ const Body = () => {
               setSearchRestro(event.target.value);
             }}
           />
-          <button
-            className="search-btn"
-            onClick={() => {
-              //Filter the Restorent here and update the UI
-              //search
-              const Searchrestrobyinput = RestolistItem.filter((RestoItem) =>
-                RestoItem.info.name
-                  .toLowerCase()
-                  .includes(searchRestro.toLowerCase())
-              );
-              setFilteredRestro(Searchrestrobyinput);
-              console.log(searchRestro);
-            }}
-          >
+
+          <button className="search-btn" onClick={handleSearch}>
             Search
           </button>
         </div>
 
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const FilterRestorent = filteredRestro.filter((Items) => {
-              return Items.info.avgRating >= 4.5;
-            });
-            setFilteredRestro(FilterRestorent);
-          }}
-        >
+        <button className="filter-btn" onClick={handleTopRestro}>
           Top Rated Restaurants
         </button>
-        <button
-          className="filter-btn"
-          onClick={() => {
-            console.log("All List");
-          }}
-        >
+
+        <button className="filter-btn" onClick={handleAllList}>
           All Restaurant
         </button>
       </div>
+
       <div className="res-container">
-        {filteredRestro?.map((restorent) => {
-          return <RestaurantCard key={restorent.info.id} resData={restorent} />;
-        })}
+
+        {filteredRestro.length !== 0 ? (
+          filteredRestro?.map((restorent) => {
+            return (
+              <RestaurantCard key={restorent.info.id} resData={restorent} />
+            );
+          })
+        ) : (
+          <h2>
+            Sorry , we couldn't find any restaurant {RestaurantName} Please
+            click on Search and add right name or see full Restaurant list
+          </h2>
+        )}
       </div>
+
     </div>
   );
 };
--- Episode6/src/Component/RestaurantCard.js
+++ Episode6/src/Component/RestaurantCard.js
@@ -3,7 +3,7 @@ import { Img_CDN_URL } from "../Utils/constant";
 const RestaurantCard = (props) => {
   const { resData } = props;
 
-  const { name, cuisines, avgRating, costForTwo, deliveryTime } = resData?.info;
+  const { name, cuisines, avgRating, costForTwo, sla } = resData?.info;
   return (
     <div className="res-card">
       <img
@@ -14,7 +14,7 @@ const RestaurantCard = (props) => {
       <h4>{cuisines.join(",")}</h4>
       <h4>{avgRating}</h4>
       <h4>{costForTwo}</h4>
-      <h4>{deliveryTime} minutes</h4>
+      <h4>{sla.slaString}</h4>
     </div>
   );
 };
