69bab2cb6d83dd742515f344fb5540786a4bf8ab
Sameer <sameerkpathanofficial@gmail.com>
2024-07-26 23:16:31 +0530

    Episode9 | Optimizing Our Code

--- Episode9/src/Utils/useRestaurantMenu.js
+++ Episode9/src/Utils/useRestaurantMenu.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Swiggy_MENU_URL } from "./constant";
 
 const useRestaurantMenu = (resID) => {
+  
   const [resMenu, setResMenu] = useState(null);
 
   useEffect(() => {
